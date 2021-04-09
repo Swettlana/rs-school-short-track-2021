8a5e08020732d865624cb032610345db7b674167
Swettlana <stellla@tut.by>
2021-04-09 15:32:28 +0300

    feat: add task 01-matrix-elements-sum

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,23 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(arr) {
+  let sum = 0;
+  // считаем сколько всего стобцов (матрица прямоуг, берем любой эл-нт)
+  let n = arr[0].length;
+  //элементы первой строки никогда не будут по 0, можно сразу посчитать их сумму
+  for (j = 0; j < n; j++) {
+    sum = sum + arr[0][j];
+  }
+  // проходимся по столбцам, а внутри по строкам ниже первой
+  for (let j = 0; j < n; j++) {
+    for (i = 1; i < arr.length; i++) {
+      if (arr[i - 1][j] !== 0) {
+        sum = sum + arr[i][j];
+      }
+    }
+  }
+  return sum;
 }
 
 module.exports = getMatrixElementsSum;
