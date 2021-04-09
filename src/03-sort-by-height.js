dd5205bdb3c2a9e9e13e210c4684451d090712aa
Swettlana <stellla@tut.by>
2021-04-09 23:42:18 +0300

    feat:add task 03-sort-by-height

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,28 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  let array = arr;
+  // создаем массив из индексов элементо "-1"
+  let arrIndex = [];
+  for (let i = 0; i < array.length; ++i) {
+    if (array[i] === -1) {
+      arrIndex.push(i);
+    }
+  }
+  //новый массив без -1
+  let arrNew = [];
+  for (let z = 0; z < array.length; z++) {
+    if (array[z] !== -1) {
+      arrNew.push(array[z]);
+    }
+  }
+  // let newArr = array.filter((el) => !(el == -1));
+  arrNew.sort((a, b) => a - b);
+  for (let j = 0; j < arrIndex.length; j++) {
+    arrNew.splice(arrIndex[j], 0, -1);
+  }
+  return arrNew;
 }
 
 module.exports = sortByHeight;
