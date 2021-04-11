c972ffcfdc82e711a86835e74cc03ce6ad44bbe7
Swettlana <stellla@tut.by>
2021-04-11 21:30:52 +0300

    feat:add task 04-mine-sweeper

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,36 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const rez = [];
+  let n = matrix.length;
+  let m = matrix[0].length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (matrix[i][j]) {
+        rez.push(1);
+      } else {
+        let count = 0;
+        for (let a = i - 1; a < i + 2; a++) {
+          for (let b = j - 1; b < j + 2; b++) {
+            try {
+              if (matrix[a][b]) {
+                count++;
+              }
+            } catch (e) {
+              continue;
+            }
+          }
+        }
+        rez.push(count);
+      }
+    }
+  }
+  const arr = [];
+  for (let i = 0; i < rez.length / m; i++) {
+    arr[i] = rez.slice(i * m, i * m + m);
+  }
+  return arr;
 }
 
 module.exports = minesweeper;
