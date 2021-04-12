e45c9cb15249ad32992629312104c3a43d84a857
Swettlana <stellla@tut.by>
2021-04-13 01:43:06 +0300

    feat: add task 11-encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,20 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const rezStr = [];
+  let sum = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      sum++;
+    } else if (sum > 1) {
+      rezStr.push(sum, str[i]);
+      sum = 1;
+    } else {
+      rezStr.push(str[i]);
+    }
+  }
+  return rezStr.join('');
 }
 
 module.exports = encodeLine;
